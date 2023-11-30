1d10afb3e30ade7dc8524b2416ec346a5ea84a57
@Draichi <lucasdraichi@gmail.com>
2023-11-30 12:02:16 -0300

    feat: implement the batching logic

--- src/model.rs
+++ src/model.rs
@@ -33,6 +33,12 @@ pub struct MNISTBatcher<B: Backend> {
     device: B::Device,
 }
 
+#[derive(Clone, Debug)]
+pub struct MNISTBatch<B: Backend> {
+    pub images: Tensor<B, 3>,
+    pub targets: Tensor<B, 1, Int>,
+}
+
 
 impl<B: Backend> MNISTBatcher<B> {
     pub fn new(device: B::Device) -> Self {
@@ -78,5 +84,31 @@ impl<B: Backend> Model<B> {
         let x = self.activation.forward(x);
 
         self.linear2.forward(x) // [batch_size, num_classes]
+    }
+}
+
+impl<B: Backend> Batcher<MNISTItem, MNISTBatch<B>> for MNISTBatcher<B> {
+    fn batch(&self, items: Vec<MNISTItem>) -> MNISTBatch<B> {
+        let images = items
+            .iter()
+            .map(|item| Data::<f32, 2>::from(item.image))
+            .map(|data| Tensor::<B, 2>::from_data(data.convert()))
+            .map(|tensor| tensor.reshape([1, 28, 28]))
+            // Normalize: make between [0,1] and make the mean=0 and std=1
+            // values mean=0.1307,sdt=0.3081 are from PyTorch MNIST example
+            // https://github.com/pytorch/examples/blob/54f4572509891883a947411fd7239237dd2a39c3/mnist/main.py#L122
+            .map(|tensor| ((tensor/255) - 0.1307) / 0.3081)
+            .collect();
+
+        let targets = items
+            .iter()
+            .map(|item| Tensor::<B, 1, Int>::from_data(Data::from([(item.label as i64).elem()])))
+            .collect();
+
+        let images = Tensor::cat(images, 0).to_device(&self.device);
+        let targets = Tensor::cat(targets, 0).to_device(&self.device);
+
+        MNISTBatch { images, targets }
+
     }
 }
